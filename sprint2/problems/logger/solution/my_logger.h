9f1a8f8c941eb2e4eb4879badeb85392450d411c
Leonid Sidorov <waltzofwoe@gmail.com>
2023-03-16 19:11:52 +0300

    logger

---
+++ sprint2/problems/logger/solution/my_logger.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <optional>
+#include <mutex>
+#include <thread>
+#include <boost/format.hpp>
+
+using namespace std::literals;
+
+#define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
+
+class Logger {
+    auto GetTime() const {
+        if (manual_ts_) {
+            return *manual_ts_;
+        }
+
+        return std::chrono::system_clock::now();
+    }
+
+    auto GetTimeStamp() const {
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        return std::put_time(std::localtime(&t_c), "%F %T");
+    }
+
+    // Для имени файла возьмите дату с форматом "%Y_%m_%d"
+    std::string GetFileTimeStamp() const {
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        std::ostringstream ss;
+        ss << "/var/log/sample_log_"s << std::put_time(std::localtime(&t_c), "%Y_%m_%d");
+        return ss.str();
+    }
+
+    Logger() = default;
+    Logger(const Logger&) = delete;
+
+public:
+    static Logger& GetInstance() {
+        static Logger obj;
+        return obj;
+    }
+
+    // Выведите в поток все аргументы.
+    template<class... Ts>
+    void Log(const Ts&... args){
+        std::lock_guard lock{m_};
+
+        std::ofstream log_file { GetFileTimeStamp(), std::ios::app };
+
+        log_file << GetTimeStamp();
+
+        for (const auto& arg : args){
+            log_file << " "s;
+            log_file << arg;
+        }
+
+        log_file << std::endl;
+    }
+
+    // Установите manual_ts_. Учтите, что эта операция может выполняться
+    // параллельно с выводом в поток, вам нужно предусмотреть 
+    // синхронизацию.
+    void SetTimestamp(std::chrono::system_clock::time_point ts){
+        std::lock_guard lock(m_);
+
+        manual_ts_ = ts;
+    }
+
+private:
+    std::optional<std::chrono::system_clock::time_point> manual_ts_;
+    std::mutex m_;
+};

7d60fb8cf1cf3cc088d31c3536d678e167571754
Leonid Sidorov <waltzofwoe@gmail.com>
2023-03-18 00:51:10 +0300

    http server

--- sprint2/problems/server_logging/solution/src/http_server.h
+++ sprint2/problems/server_logging/solution/src/http_server.h
@@ -56,13 +56,14 @@ private:
 template <typename RequestHandler>
 class Session : public SessionBase, public std::enable_shared_from_this<Session<RequestHandler>> {
 public:
-    Session(tcp::socket&& socket, std::shared_ptr<RequestHandler> handler)
+    template<typename Handler>
+    Session(tcp::socket&& socket, Handler&& request_handler)
         : SessionBase(std::move(socket)),
-          request_handler_(handler){
+          request_handler_(std::forward<Handler>(request_handler)){
     }
 
 private:
-    std::shared_ptr<RequestHandler> request_handler_;
+    RequestHandler request_handler_;
 
     std::shared_ptr<SessionBase> GetSharedThis() override {
         return this->shared_from_this();
@@ -76,10 +77,11 @@ private:
 template <typename RequestHandler>
 class Listener : public std::enable_shared_from_this<Listener<RequestHandler>> {
 public:
-    Listener(net::io_context& ioc, const tcp::endpoint& endpoint, std::shared_ptr<RequestHandler> request_handler)
+    template<typename Handler>
+    Listener(net::io_context& ioc, const tcp::endpoint& endpoint, Handler&& request_handler)
         : ioc_(ioc),
           acceptor_(net::make_strand(ioc)),
-          request_handler_(request_handler) {
+          request_handler_(std::forward<Handler>(request_handler)) {
         acceptor_.open(endpoint.protocol());
 
         acceptor_.set_option(net::socket_base::reuse_address(true));
@@ -95,8 +97,8 @@ public:
     
 private:
     net::io_context& ioc_;
-    tcp::acceptor acceptor_{};
-    std::shared_ptr<RequestHandler> request_handler_;
+    tcp::acceptor acceptor_{net::make_strand(ioc_)};
+    RequestHandler request_handler_;
 
     void DoAccept() {
         acceptor_.async_accept(net::make_strand(ioc_), 
@@ -119,10 +121,10 @@ private:
 };
 
 template <typename RequestHandler>
-void ServeHttp(net::io_context& ioc, const tcp::endpoint& endpoint, std::shared_ptr<RequestHandler> handler) {
+void ServeHttp(net::io_context& ioc, const tcp::endpoint& endpoint, RequestHandler&& handler) {
     using MyListener = Listener<std::decay_t<RequestHandler>>;
 
-    std::make_shared<MyListener>(ioc, endpoint, handler)->Run();
+    std::make_shared<MyListener>(ioc, endpoint, std::forward<RequestHandler>(handler))->Run();
 }
 
 }  // namespace http_server

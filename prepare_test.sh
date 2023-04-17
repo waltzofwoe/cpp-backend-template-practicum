72744851723d6bb9ee19896c85ac1fc8aab92a6e
Alexey Konstantinov <konstantinov.py@gmail.com>
2023-04-17 05:08:55 +0300

    Add scripts for local test run

--- prepare_test.sh
+++ prepare_test.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
 FOLDER="cpp-backend-tests-practicum"
-REPO=${PWD}
+
+function real_dir() {
+  pushd "$1" >/dev/null
+  pwd -P
+  popd >/dev/null
+}
+REPO=$(real_dir "$(dirname "$0")")
+
 cd ${REPO} || exit 1
-if [ ! -d "$FOLDER" ] ; then
-  git clone git@github.com:cpppracticum/"$FOLDER".git "$FOLDER"
-else
+
+if ! git clone git@github.com:cpppracticum/"$FOLDER".git "$FOLDER"; then
   (
   cd "$FOLDER" || exit 1
   git pull
@@ -14,6 +22,7 @@ fi
 if [ ! -d "${REPO}/.venv" ] ; then
   python3 -m venv ${REPO}/.venv
 fi
-  source ${REPO}/.venv/bin/activate
-  python3 -m pip install --upgrade pip
-  python3 -m pip install -r requirements.txt
+
+source ${REPO}/.venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt

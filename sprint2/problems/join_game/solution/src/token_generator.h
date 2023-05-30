2542fc3f8bf8e760165fb30b03b311f6dc49aa79
Сидоров Леонид <sidorovlv@it2g.ru>
2023-05-30 17:58:15 +0300

    fix

--- sprint2/problems/join_game/solution/src/token_generator.h
+++ sprint2/problems/join_game/solution/src/token_generator.h
@@ -30,9 +30,7 @@ namespace tokens {
 
         std::ostringstream str;
 
-        str << std::hex << std::setfill('0') << std::setw(sizeof(uint_fast64_t)) << value1 << value2;
-
-        str.flush();
+        str << std::hex << std::setfill('0') << std::setw(sizeof(uint_fast64_t)*2) << value1 << value2;
 
         return str.str();
     }

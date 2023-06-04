069b7467dfbce56a6854737c5314fffabe51890c
Сидоров Леонид <sidorovlv@it2g.ru>
2023-06-04 22:33:46 +0300

    fix

--- sprint2/problems/time_control/solution/src/application.cpp
+++ sprint2/problems/time_control/solution/src/application.cpp
@@ -199,7 +199,7 @@ std::vector<Collision> Application::GetCollisionsAtPosition(model::Position& pos
             c.x_min = a.x < b.x ? a.x : b.x;
             c.x_max = a.x > b.x ? a.x : b.x;
             c.y_min = a.y < b.y ? a.y : b.y;
-            c.y_max = a.y < b.y ? a.y : b.y;
+            c.y_max = a.y > b.y ? a.y : b.y;
 
             result.emplace_back(c);
         }

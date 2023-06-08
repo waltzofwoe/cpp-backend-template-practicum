91b18b461e1033f5539180ba1a689fce95811cf7
Сидоров Леонид <sidorovlv@it2g.ru>
2023-06-08 18:16:20 +0300

    sprint3 flamegraph

---
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -0,0 +1,63 @@
+import argparse
+import subprocess
+import time
+import random
+import shlex
+
+RANDOM_LIMIT = 1000
+SEED = 123456789
+random.seed(SEED)
+
+AMMUNITION = [
+    'localhost:8080/api/v1/maps/map1',
+    'localhost:8080/api/v1/maps'
+]
+
+SHOOT_COUNT = 100
+COOLDOWN = 0.1
+
+
+def start_server():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server', type=str)
+    return parser.parse_args().server
+
+def start_perf(pid):
+    return "perf record -p {pid} -o perf.data"
+
+def start_flamegraph():
+    return "perf script -i perf.data | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl"
+
+def run(command, output=None):
+    process = subprocess.Popen(shlex.split(command), stdout=output, stderr=subprocess.DEVNULL)
+    return process
+
+
+def stop(process, wait=False):
+    if process.poll() is None and wait:
+        process.wait()
+    process.terminate()
+
+
+def shoot(ammo):
+    hit = run('curl ' + ammo, output=subprocess.DEVNULL)
+    time.sleep(COOLDOWN)
+    stop(hit, wait=True)
+
+
+def make_shots():
+    for _ in range(SHOOT_COUNT):
+        ammo_number = random.randrange(RANDOM_LIMIT) % len(AMMUNITION)
+        shoot(AMMUNITION[ammo_number])
+    print('Shooting complete')
+
+
+server = run(start_server())
+perf = run(start_perf(server.pid))
+make_shots()
+stop(perf)
+stop(server)
+flamegraph = run(start_flamegraph(), "graph.svg")
+flamegraph.wait()
+time.sleep(1)
+print('Job done')
